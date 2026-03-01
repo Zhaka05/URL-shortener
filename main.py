897ee0624106c602564e7679d32ca9a6e1ff9900
Zhaka <shingis.zhansen@gmail.com>
2026-03-01 04:16:51 -0600

    add two endpoints and import modules for short url generation

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from url-shortener!")
+import string
+from secrets import choice
 
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+ALPHABET = string.ascii_letters + string.digits
+
+@app.post("/short_url")
+async def generate_short_url():
+    return ...
+
+@app.get("/{slug}")
+async def redirect_to_url(slug: str):
+    return ...
+
